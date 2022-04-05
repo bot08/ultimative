85755782c6dc9949087119b1e0ae9efcee0cd665
Oleg <scherbaoleg4@gmail.com>
2022-04-05 14:35:28 +0300

    Update menu.php

--- views/system/menu.php
+++ views/system/menu.php
@@ -1,3 +1,11 @@
 <h2>Меню</h2>
-<a href="/"class="font-color">Home</a><br>
-<a href="/github" class="font-color">Github</a>
+<a href="/"class="font-color">
+    <h4>
+        Home
+    </h4>
+</a>
+<a href="/github"class="font-color">
+    <h4>
+        Github
+    </h4>
+</a>
